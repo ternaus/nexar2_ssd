63763479c5291e7757f1bebfe06f2b801a2b49f1
Ellis Brown <elbrown47@gmail.com>
2017-04-26 18:59:52 -0500

    added caching for server ip

---
+++ demos/live/cache.py
@@ -0,0 +1,64 @@
+""" cache.py
+
+sets up the demo with the server ip address
+
+Ellis Brown, Max deGroot
+"""
+
+
+import argparse
+import ipaddress
+import os.path
+import sys
+
+IP_FILE = 'ip_cache.txt'
+
+
+def parse_args(argv=None):
+    """parses arg values"""
+    parser = argparse.ArgumentParser(
+        description="""Configure demo example with Server IP address.
+        (note: the previous ip will be reloaded if none is passed)""")
+    parser.add_argument('ip', nargs='?',
+                        help='(optional) ip address of server machine')
+    return parser.parse_args(argv)
+
+
+def load_ip():
+    """loads the previous ip, assuming no ip passed in"""
+    assert os.path.isfile(IP_FILE), IP_FILE + " not found and no ip supplied."
+    with open(IP_FILE, "r") as f:
+        ip = f.read()
+    return ipaddress.ip_address(ip.strip())
+
+
+def cache_ip(ip):
+    """stores the server ip addr in the IP_FILE"""
+    with open(IP_FILE, "w") as f:
+        f.write(ip)
+
+
+def server_ip(address=None):
+    """returns the server ip for use with live demo. caches ip's in IP_FILE.
+
+    Args:
+        address (optional, string): ip address of server machine
+            if supplied: cache it in IP_FILE
+            else: load ip from IP_FILE
+    Return:
+        ipaddress (ipaddress.IPv4Address OR ipaddress.IPv6Address)
+            type of ipaddress will depend on format of ip supplied
+    """
+    if address:
+        # ip address supplied, store it for next run
+        cache_ip(address)
+        ip = ipaddress.ip_address(address)
+    else:
+        ip = load_ip()
+    print('Using server ip:', ip)
+    return ip
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    server_ip(args.ip)
--- demos/live/client.py
+++ demos/live/client.py
@@ -5,55 +5,61 @@ Ellis Brown, Max deGroot
 
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
-import cv2
+# import cv2
 import numpy as np
 import torch
 import time
 import struct
+
+from cache import server_ip, parse_args
 #
 # transform = BaseTransform(net.size, (104, 117, 123))
 # colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 # font = cv2.FONT_HERSHEY_SIMPLEX
 
-SERVER_IP = ''
-PORT_NUMBER = 5000
-SIZE = 4096
-print("Test client sending packets to IP {0}, via port {1}\n"
-      .format(SERVER_IP, PORT_NUMBER))
+args = parse_args()
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((SERVER_IP, PORT_NUMBER))
 
-video_capture = cv2.VideoCapture(0)
-while True:
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    data = pickle.dumps(frame)
-    # s.sendall(struct.pack("L", len(data))+data)
-    ret, jpeg = cv2.imencode('.jpg', frame)
-    jpeg = jpeg.tobytes()
-    s.send((b'--frame\r\n'
-		    b'Content-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n\r1\n'))
-    # try:
-    #     s.send(jpeg.tostring())
-    #     data = s.recv(SIZE)
-    #     i = cv2.imdecode(np.fromstring(data, dtype=np.float64), cv2.IMREAD_COLOR)
-    #     print(i)
-    # except ConnectionResetError:
-    #     print('connection reset')
-    data = s.recv(SIZE)
-    # Display the resulting frame
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    cv2.imshow('Video', i)
-    cv2.waitKey(1)
-
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
-s.close()
-sys.exit()
+SERVER_IP = server_ip(args.ip)
+print("SERVER_IP:",SERVER_IP)
+# PORT_NUMBER = 5000
+# SIZE = 4096
+# print("Test client sending packets to IP {0}, via port {1}\n"
+#       .format(SERVER_IP, PORT_NUMBER))
+#
+# s = socket(AF_INET, SOCK_STREAM)
+# s.connect((SERVER_IP, PORT_NUMBER))
+#
+# video_capture = cv2.VideoCapture(0)
+# while True:
+#     if not video_capture.isOpened():
+#         print('Unable to load camera.')
+#         sleep(5)
+#         pass
+#     # Capture frame-by-frame
+#     ret, frame = video_capture.read()
+#     data = pickle.dumps(frame)
+#     # s.sendall(struct.pack("L", len(data))+data)
+#     ret, jpeg = cv2.imencode('.jpg', frame)
+#     jpeg = jpeg.tobytes()
+#     s.send((b'--frame\r\n'
+# 		    b'Content-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n\r1\n'))
+#     # try:
+#     #     s.send(jpeg.tostring())
+#     #     data = s.recv(SIZE)
+#     #     i = cv2.imdecode(np.fromstring(data, dtype=np.float64), cv2.IMREAD_COLOR)
+#     #     print(i)
+#     # except ConnectionResetError:
+#     #     print('connection reset')
+#     data = s.recv(SIZE)
+#     # Display the resulting frame
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#     cv2.imshow('Video', i)
+#     cv2.waitKey(1)
+#
+# # When everything is done, release the capture
+# video_capture.release()
+# cv2.destroyAllWindows()
+# s.close()
+# sys.exit()
