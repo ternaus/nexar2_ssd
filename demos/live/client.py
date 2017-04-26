2ab5de1a2666bef0ea9f1e8214ac56e4d9dd0822
Max deGroot <alexander.m.degroot@vanderbilt.edu>
2017-04-26 16:04:52 -0500

    working  socket

--- demos/live/client.py
+++ demos/live/client.py
@@ -4,24 +4,27 @@ Ellis Brown, Max deGroot
 """
 
 import sys
-import socket
 from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 
-SERVER_IP  = ''
+SERVER_IP = ''
 PORT_NUMBER = 5000
-SIZE = 20
-print("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
+SIZE = 1024
+print("Test client sending packets to IP {0}, via port {1}\n"
+      .format(SERVER_IP, PORT_NUMBER))
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((SERVER_IP, PORT_NUMBER))
 
 message = "cool"
 
-
-s.send(message.encode())
-data = s.recv(SIZE)
+while True:
+    try:
+        s.send(message.encode())
+        data = s.recv(SIZE).decode()
+    except ConnectionResetError:
+        print('connection reset')
+    print(data)
 s.close()
-print(data)
 sys.exit()
 
 # while True:
--- demos/live/server.py
+++ demos/live/server.py
@@ -4,9 +4,8 @@ Ellis Brown, Max deGroot
 """
 
 import sys
-import socket
+from socket import socket, gethostbyname, AF_INET, SOCK_STREAM
 
-from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM, SOCK_STREAM
 PORT_NUMBER = 5000
 SIZE = 1024
 
@@ -14,17 +13,17 @@ hostName = gethostbyname('0.0.0.0')
 
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((hostName, PORT_NUMBER))
-s.listen(1)
+s.listen(2)
 
 conn, addr = s.accept()
-# print("Test server listening on port {0}\n".format(PORT_NUMBER))
-# print("Connection address: ", addr)
+print("Test server listening on port {0}\n".format(PORT_NUMBER))
+print("Connection address: ", addr)
 
 while True:
-        data = s.recv(SIZE)
+        data = conn.recv(SIZE).decode()
         if not data:
             break
         print("data received: ",  data)
-        conn.send(data)
+        conn.send(data.encode())
 
 sys.exit()
