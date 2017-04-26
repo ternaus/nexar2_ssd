2e1844f563e160065ec0f53f6c9d46125f908687
Max deGroot <alexander.m.degroot@vanderbilt.edu>
2017-04-26 15:48:48 -0500

    update client and server

--- demos/live/client.py
+++ demos/live/client.py
@@ -5,3 +5,25 @@ Ellis Brown, Max deGroot
 
 import sys
 import socket
+from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
+
+SERVER_IP  = ''
+PORT_NUMBER = 5000
+SIZE = 20
+print("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect((SERVER_IP, PORT_NUMBER))
+
+message = "cool"
+
+
+s.send(message.encode())
+data = s.recv(SIZE)
+s.close()
+print(data)
+sys.exit()
+
+# while True:
+#         mySocket.sendto(message.encode(), (SERVER_IP, PORT_NUMBER))
+# sys.exit()
--- demos/live/server.py
+++ demos/live/server.py
@@ -6,4 +6,25 @@ Ellis Brown, Max deGroot
 import sys
 import socket
 
+from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM, SOCK_STREAM
+PORT_NUMBER = 5000
+SIZE = 1024
 
+hostName = gethostbyname('0.0.0.0')
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((hostName, PORT_NUMBER))
+s.listen(1)
+
+conn, addr = s.accept()
+# print("Test server listening on port {0}\n".format(PORT_NUMBER))
+# print("Connection address: ", addr)
+
+while True:
+        data = s.recv(SIZE)
+        if not data:
+            break
+        print("data received: ",  data)
+        conn.send(data)
+
+sys.exit()
