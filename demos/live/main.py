5c8ad7dd8cb32ea815c68385c1bc0ae41ab99262
Max deGroot <alexander.m.degroot@vanderbilt.edu>
2017-04-26 18:52:24 -0500

    update live demo

---
+++ demos/live/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, Response
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        success, image = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        print('hi')
+        return jpeg.tobytes()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+			     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r1\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+	return Response(gen(VideoCamera()),
+		mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+	app.run(host='', debug=True, threaded=True)
--- demos/live/server.py
+++ demos/live/server.py
@@ -4,26 +4,84 @@ Ellis Brown, Max deGroot
 """
 
 import sys
+import time
 from socket import socket, gethostbyname, AF_INET, SOCK_STREAM
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from sys import platform as sys_pf
+from data import VOC_CLASSES as labelmap
+import torch.utils.data as data
+from data import BaseTransform
+from ssd import build_ssd
+import cv2
+import numpy as np
+import struct
+import pickle
 
 PORT_NUMBER = 5000
-SIZE = 1024
-
+SIZE = 4096
 hostName = gethostbyname('0.0.0.0')
+
+trained_model = 'weights/ssd_300_VOC0712.pth'
+net = build_ssd('test', 300, 21)    # initialize SSD
+net.load_state_dict(torch.load(trained_model))
+net = net.cuda()
+cudnn.benchmark = True
+net.eval()
+transform = BaseTransform(net.size, (104, 117, 123))
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+def predict(frame):
+    height, width, _ = frame.shape
+    x = Variable(transform(frame).unsqueeze(0).cuda(), volatile=True)
+    y = net(x)  # forward pass
+    detections = y.data
+    # scale each detection back up to the image
+    scale = torch.Tensor([width, height, width, height])
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0, i, j, 0] >= 0.4:
+            pt = (detections[0, i, j, 1:]*scale).cpu().numpy()
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]),
+                          int(pt[3])), colors[i % 3], 2)
+            cv2.putText(frame, labelmap[i-1], (int(pt[0]), int(pt[1])), font,
+                        2, (255, 255, 255), 2, cv2.LINE_AA)
+            j += 1
+    return frame
+
 
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((hostName, PORT_NUMBER))
 s.listen(2)
-
 conn, addr = s.accept()
 print("Test server listening on port {0}\n".format(PORT_NUMBER))
 print("Connection address: ", addr)
 
+data = ''
+payload_size = struct.calcsize("L")
 while True:
-        data = conn.recv(SIZE).decode()
-        if not data:
-            break
-        print("data received: ",  data)
-        conn.send(data.encode())
+    while len(data) < payload_size:
+        data += conn.recv(SIZE)
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("H", packed_msg_size)[0]
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    # i = cv2.imdecode(np.fromstring(data, dtype=np.float64), cv2.IMREAD_COLOR)
+    if i is None:
+        continue
+    data = predict(i)
+    print(data)
+    ret, jpeg = cv2.imencode('.jpg', data)
+    conn.send(jpeg.tostring())
+
 
 sys.exit()
