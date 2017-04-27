9c2cd87d594dca75f68f43e7aaebd5e4a79b10f2
Ellis Brown <elbrown47@gmail.com>
2017-04-27 07:32:12 -0500

    multithreaded video loading with 'p' pause and 'esc' exit, fps tracking

--- demos/live/client.py
+++ demos/live/client.py
@@ -4,57 +4,60 @@ Ellis Brown, Max deGroot
 """
 
 import sys
-from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
+import socket
 import cv2
 import numpy as np
-import torch
 import time
-import struct
+from imutils.video import FPS, WebcamVideoStream
+
 
 from cache import server_ip, parse_args
 
-args = parse_args()
+# networking
+# args = parse_args()
+# SERVER_IP = str(server_ip(args.ip))
+# PORT = 5000
+# SIZE = 3200
+# print("Test client sending packets to IP {0}, via port {1}\n"
+#       .format(SERVER_IP, PORT))
+#
+# sock = socket.socket(socket.AF_INET,     # Internet
+#                      socket.SOCK_DGRAM)  # UDP
+# sock.bind((SERVER_IP, PORT))
 
-SERVER_IP = str(server_ip(args.ip))
-print("SERVER_IP:", SERVER_IP)
-PORT_NUMBER = 5000
-SIZE = 3200
-print("Test client sending packets to IP {0}, via port {1}\n"
-      .format(SERVER_IP, PORT_NUMBER))
+# start video stream thread, allow buffer to fill
+print("[INFO] starting threaded video stream...")
+stream = WebcamVideoStream(src=0).start()  # default camera
+time.sleep(1.0)
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((SERVER_IP, PORT_NUMBER))
+# start fps timer
+fps = FPS().start()
 
-video_capture = cv2.VideoCapture(0)
+# loop over frames from the video file stream
 while True:
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    # data = pickle.dumps(frame)
-    # s.sendall(struct.pack("L", len(data))+data)
-    ret, jpeg = cv2.imencode('.jpg', frame)
-    # jpeg = jpeg.tobytes()
-    # s.send((b'--frame\r\n'
-    # 	    b'Content-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n\r1\n'))
-    try:
-        s.send(jpeg.tostring())
-        data = s.recv(SIZE)
-        i = cv2.imdecode(np.fromstring(
-            data, dtype=np.float32), cv2.IMREAD_COLOR)
-        print(i)
-    except ConnectionResetError:
-        print('connection reset')
-    # Display the resulting frame
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    cv2.imshow('Video', i)
-    cv2.waitKey(1)
+    # grab next frame
+    frame = stream.read()
+    key = cv2.waitKey(1) & 0xFF
 
-# When everything is done, release the capture
-video_capture.release()
+    # update FPS counter
+    fps.update()
+
+    # keybindings
+    if key == ord('p'):  # pause
+        while True:
+            key2 = cv2.waitKey(1) or 0xff
+            cv2.imshow('frame', frame)
+            if key2 == ord('p'):  # resume
+                break
+    cv2.imshow('frame', frame)
+    if key == 27:  # exit
+        break
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# cleanup
 cv2.destroyAllWindows()
-s.close()
-sys.exit()
+stream.stop()
